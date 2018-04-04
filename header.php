b0bd6a68c1cc132df5341e8169ab119904dda489
Alias511 <forinfo511@gmail.com>
2018-04-04 15:12:21 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+      <title>Carousel Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+   <link href="css/bootstrap.css" rel="stylesheet">
+   <link href="css/carousel.css" rel="stylesheet">
+   <link rel="stylesheet" href="css/jquery-ui.css">
+   
+   <link rel="stylesheet" href="css/superfish.css/superfish.css">
+   <link rel="stylesheet" href="css/superfish.css/superfish-navbar.css">
+   <link rel="stylesheet" href="css/superfish.css/superfish-vertical.css">
+   
+   <link rel="stylesheet" href="js/superfish.js/superfish.min.js">
+   <link rel="stylesheet" href="css/superfish.css/megafish.css">
+    <link rel="stylesheet" href="css/colorboxCSS/colorbox.css">
+    <link rel="stylesheet" href="css/bxSliderCSS/jquery.bxslider.css">
+    <link rel="stylesheet" href="engine1/style.css">
+   <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Carousel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="effects.php">Effects JqueryUI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="widgets.php">Widgets JqueryUI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="actions.php">Actions JqueryUI</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="superfish.php">Menu Superfish</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="#">Gallery</a>
+               <li class="nav-item">
+              <a class="nav-link disabled" href="bxslider.php">BX Slider</a>
+              <li class="nav-item">
+              <a class="nav-link disabled" href="wowslider.php">Wow Slider</a>
+            </li>
+            <!-- <li class="nav-item">
+              <a class="nav-link disabled" href="forma.php">Обратная связь</a>
+            </li> перенесли в dropdown-menu -->
+            </li>
+            </li>
+                                       v                                                                                                                                                                                                                                                                   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv >
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Кнопка выпадающего списка
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="forma.php">Обратная связь</a>
+    <a class="dropdown-item" href="phpforma.php">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
